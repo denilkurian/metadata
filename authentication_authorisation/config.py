930a6eba2f56c2e632130e14063f4be0f8c5583b
Denil Kurian <122264994+denilkurian@users.noreply.github.com>
2023-10-07 18:49:16 +0530

    Update config.py
    New

--- authentication_authorisation/config.py
+++ authentication_authorisation/config.py
@@ -1,5 +1,5 @@
-GOOGLE_CLIENT_ID = "776635407081-a1dhm9214g8cqatujv7rsbcbge5mff8b.apps.googleusercontent.com"
+GOOGLE_CLIENT_ID =
 
-GOOGLE_CLIENT_SECRET = "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"
+GOOGLE_CLIENT_SECRET = 
 
 
