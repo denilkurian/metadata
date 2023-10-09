1b8e748a41fb097305f3ceba15d99f70f82dffa0
denilkurian <denilkuriancc@gmail.com>
2023-10-09 11:39:09 +0530

    requirement.txt

--- authentication_authorisation/auth_api/google_auth.py
+++ authentication_authorisation/auth_api/google_auth.py
@@ -12,9 +12,7 @@ import requests
 from sqlalchemy.orm import Session
 
 
-
 router = APIRouter()
-
 
 
 @router.get("/auth/google")
@@ -25,6 +23,7 @@ def google_oauth_login():
     redirect_uri = "http://127.0.0.1:8000/auth/google/callback"
     scope = "openid profile email"  # Define required scopes
     return RedirectResponse(f"{google_oauth_url}?client_id={client_id}&redirect_uri={redirect_uri}&scope={scope}&response_type=code")
+
 
 
 GOOGLE_REDIRECT_URI = "http://127.0.0.1:8000/auth/google/callback"
@@ -42,44 +41,14 @@ def exchange_code_for_access_token(code: str) -> dict:
     return response.json()
 
 
+
 @router.get("/auth/google/callback")
-async def google_oauth_callback(
-    request: Request,
-    code: str = Query(...),
-    db: Session = Depends(lambda r: SessionLocal())
-):
-    # Exchange the authorization code for an access token
-    google_auth_data = exchange_code_for_access_token(code)
-    
-    # Fetch user data from Google API
-    google_user_data = requests.get("https://www.googleapis.com/oauth2/v2/userinfo", 
-        headers={"Authorization": f"Bearer {google_auth_data['access_token']}"}
-    ).json()
-    
-    # Check if the user already exists in the database
-    existing_google_user = db.query(GoogleAuthUser).filter_by(google_id=google_user_data['id']).first()
-    
-    if existing_google_user:
-        # User already exists, log them in
-        user = existing_google_user.user
-    else:
-        # Create a new user in the database
-        user = User(
-            first_name=google_user_data['given_name'],
-            last_name=google_user_data['family_name'],
-            email=google_user_data['email'],
-        )
-        db.add(user)
-        db.commit()
-        
-        # Create a GoogleAuthUser entry to link the user with their Google account
-        google_auth_user = GoogleAuthUser(google_id=google_user_data['id'], user=user)
-        db.add(google_auth_user)
-        db.commit()
-    
-    # Implement your own user authentication logic here if needed
-    # For simplicity, we're just returning a message
-    return {"message": "Successfully logged in with Google"}
+async def google_oauth_callback(request: Request):
+    authorization_code = request.query_params.get("code")
+    print("Authorization Code:", authorization_code)
+
+
+
 
 
 
